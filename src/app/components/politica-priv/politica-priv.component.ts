b86130569b1fcecf2823e76128580238f72606b7
Verónica De Luca <veronica.delucatalavera.externo@swissmedicalgroup.onmicrosoft.com>
2025-04-29 16:42:07 -0300

    finalizacion pantalla

--- src/app/components/politica-priv/politica-priv.component.ts
+++ src/app/components/politica-priv/politica-priv.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { WpButtonComponent } from "../wp-button/wp-button.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-politica-priv',
@@ -9,6 +10,14 @@ import { WpButtonComponent } from "../wp-button/wp-button.component";
   templateUrl: './politica-priv.component.html',
   styleUrl: './politica-priv.component.css'
 })
-export class PoliticaPrivComponent {
+export class PoliticaPrivComponent implements OnInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo(0, 0);
+    }
+  }
 
 }
